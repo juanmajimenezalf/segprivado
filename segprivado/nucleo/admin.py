4db3ceb6287db3827027df32febd6f4cfc87eed4
juanmajimenezalf <jimenez.aljua20@cadiz.salesianos.edu>
2022-06-01 23:40:12 +0200

    Update admin.py

--- segprivado/nucleo/admin.py
+++ segprivado/nucleo/admin.py
@@ -1,9 +1,23 @@
+from django import forms
 from django.contrib import admin
 
-from .models import Usuario
+from .models import Medicamento, Usuario
 
 class UsuarioAdmin(admin.ModelAdmin):
    fields = ['first_name', 'last_name', 'direccion', 'email', 'especialidad', 'username', 'password', 'is_medico', 'is_paciente', 'is_active' ]
 
+class MedicamentoAdminForm(forms.ModelForm):
+   def clean_receta(self):
+      receta = self.cleaned_data['receta']
+      if receta != 'S' and receta != 'N':
+         raise forms.ValidationError("Debe ser S o N")
+      else:
+         return receta
+
+class MedicamentoAdmin(admin.ModelAdmin):
+   form = MedicamentoAdminForm
+   search_fields = ['nombre']
+         
 # Register your models here.
-admin.site.register(Usuario, )
+admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Medicamento, MedicamentoAdmin)
